8b717213cf8aeb1e3775e8164a4dd0f5e90edd7e
cedricvmol <vanmolcedric@hotmail.com>
2026-06-03 14:30:53 +0200

    KAN-47

--- src/main/java/service/LoanService.java
+++ src/main/java/service/LoanService.java
@@ -36,6 +36,18 @@ public class LoanService {
         return loans.stream().filter(loan -> !loan.isReturned()).collect(Collectors.toList());
     }
 
+    public String returnBook(String loanId){
+        BookLoan loan = loans.stream().filter(bookLoan -> bookLoan.getLoanId().equals(loanId)).findFirst().orElseThrow(() -> new IllegalArgumentException("No loan was found with that ID."));
+        if (loan.isReturned()){
+            throw new IllegalArgumentException("The book is already returned.");
+        }
+        loan.setReturned(true);
+        loan.setReturnDate(LocalDate.now());
+        loan.getCopy().setStatus(CopyStatus.AVAILABLE);
+
+        return loan.getCopy().getBook().getIsbn();
+    }
+
     public String loanIdGenerator() {
         Random random = new Random();
         StringBuilder id = new StringBuilder();
